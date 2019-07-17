86f3127bc2210eb4630653c0dc4c48d290259f40
Pete Travis <immanetize@fedoraproject.org>
2019-07-17 08:44:05 -0500

    make it a setuptools project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='routeserviced',
+    description='be confined no more, walled hosts',
+    version='0.1',
+    url='https://github.com/immanetize/routeserviced',
+    author='Pete Travis',
+    author_email='github@petetravis.com',
+    license='GPLv2',
+    packages=['routeserviced'],
+    zip_safe=False
+    )
